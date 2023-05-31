2361e3c5353058bb29c8bb3c54d2091603cbfeb6
Ian Roberts <ian.roberts@sansano.usm.cl>
2023-05-31 15:02:02 -0400

    Changed Delta-Hiperbolicity Feature Extractor

--- delta_hyperbolicity.py
+++ delta_hyperbolicity.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 
-from tqdm import tqdm
+from torchsummary import summary
 from scipy.spatial import distance_matrix
 
 import torch
@@ -10,8 +10,11 @@ import torch.nn as nn
 import torchvision.models as models
 
 from datasets.CIFAR10 import generate_CIFAR10
-from datasets.CIFAR100 import generate_CIFAR100 
+from datasets.CIFAR100 import generate_CIFAR100
+from networks.vgg16 import create_vgg16_model as network
 from metrics.mAP import get_embeddings
+
+np.random.seed(42)
 
 # Check CUDA availability to use GPU
 cuda = torch.cuda.is_available()
@@ -24,16 +27,6 @@ else:
     print('[INFO] CUDA is not available. Using CPU...')
     NUM_WORKERS = 4
     PIN_MEMORY = False
-
-
-def create_feature_extractor_vgg16_bn(device):
-    model = models.vgg16_bn(weights='IMAGENET1K_V1')
-    model_features = model.features
-    model_classifier = nn.Sequential(*list(model.classifier.children())[:-3])
-
-    feature_extractor = nn.Sequential(model_features, nn.Flatten(), model_classifier).to(device)
-
-    return feature_extractor
 
 
 def delta_hyp(dist_matrix):
@@ -50,16 +43,17 @@ def delta_hyp(dist_matrix):
     return np.max(maxmin - XY_p)
 
 
-def get_delta(loader, device):
+def get_delta(model, loader, device):
     """
     Computes the delta-hyperbolicity value for image data by extracting features using VGG network.
     """
-    feature_extractor = create_feature_extractor_vgg16_bn(device)
-
     embeddings, _ = get_embeddings(feature_extractor, loader, device)
 
     print('[INFO] Calculating Distance Matrix')
     dists = distance_matrix(embeddings, embeddings)
+
+    idx = np.random.choice(len(embeddings), 1500)
+    dists = embeddings[idx]
 
     delta = delta_hyp(dists)
     diam = np.max(dists)
@@ -67,6 +61,15 @@ def get_delta(loader, device):
 
 # Generate the loaders for the CIFAR10 dataset
 train_loader, validation_loader, test_loader, classes, superclasses = generate_CIFAR10(validation_size=0.0)
+num_classes = 10
+
+# Generate the feature extractor model
+# We use a curvature of 1.0 and dimension of 32 as these present the best euclidean results for our model
+model = network(1.0, 32, False, True, num_classes=num_classes)
+model = model.to(device)
+
+model.load_state_dict(torch.load('./weights/euclidean/CIFAR10_VGG16_32.pth'), strict=False)
+summary(model, (3, 32, 32))
 
 delta, diam = get_delta(train_loader, device)
 print('Delta-Hiperbolicity:', delta)
